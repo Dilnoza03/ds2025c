14069a54880d0531697dea6e779ddcd5c2082919
Dilnoza <dilnozaakhmedzhanova7@gmail.com>
2025-03-24 14:27:11 +0900

    v0.6 array

--- week03.py
+++ week03.py
@@ -1,8 +1,16 @@
-import array
-
-arr = array.array('f', [99, 0, -7, 0, 16])
-for i in range(len(arr)):
-    print(f"{arr[i]:5} {id(arr[i])}")
+def move_zeros(l):
+    zero_idx = 0
+    for i in range(len(l)):
+        n = l[i]
+        if n != 0:
+            l[zero_idx] = n
+            if zero_idx != i:
+                l[i] = 0
+            zero_idx = zero_idx + 1
+    return l
+l = [99, 0, -7, 0, 16]
+move_zeros(l)
+print(l)
 
 
 
