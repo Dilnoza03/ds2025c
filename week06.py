0767bf3fccd5c31fa3d99655b21e8e17171844df
Dilnoza <dilnozaakhmedzhanova7@gmail.com>
2025-04-14 14:24:50 +0900

    v2.6 queue

--- week06.py
+++ week06.py
@@ -20,7 +20,24 @@ class Queue:
             self.rear.link = node # связываем старый "хвост" с новым узлом
             self.rear = node   # новый "хвост" теперь — добавленный узел
 
+
+    def dequeue(self):
+        if self.front is None:
+            raise IndexError('Queue is empty') # если очередь пуста — ошибка
+        self.size = self.size - 1
+        temp = self.front  # сохраняем текущий первый элемент
+        self.front = self.front.link  # передвигаем front на следующий элемент
+        if self.front is None:
+            self.rear = None  # если очередь стала пустой — обнуляем rear
+        return temp.data
+
 q = Queue()
 q.enqueue("Database")
 q.enqueue("Data structure")
 print(q.size, q.front.data, q.rear.data)
+print(q.dequeue())
+print(q.size, q.front.data, q.rear.data)
+print(q.dequeue())
+print(q.size, q.front, q.rear)
+# print(q.dequeue())
+
