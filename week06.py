e8e0a77b91ea4f8ff5202ea402e4db5ee59a7bb6
Dilnoza <dilnozaakhmedzhanova7@gmail.com>
2025-04-14 15:07:59 +0900

    v2.9 queue

--- week06.py
+++ week06.py
@@ -1,45 +1,14 @@
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data  # значение (например, "Database")
-        self.link = link   # ссылка на следующий узел
-
-
-class Queue:
-    def __init__(self):
-        self.front = None  # первый элемент очереди
-        self.rear = None  # последний элемент очереди
-        self.size = 0  # количество элементов
-
-    def enqueue(self, data): #добовляет в очкередь
-        self.size = self.size + 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.link = node
-            self.rear = node
-
-
-    def dequeue(self): #это удаляет очередь
-        if self.front is None:
-            raise IndexError('Queue is empty')
-        self.size = self.size - 1
-        temp = self.front
-        if self.front is None:
-            self.rear = None
-        temp.link = None
-        return temp.data
+from queue import Queue
 
 q = Queue()
-q.enqueue("Database")
-q.enqueue("Data structure")
-q.enqueue("Java script")
-print(q.size, q.front.data, q.rear.data)
-print(q.dequeue())
-print(q.size, q.front.data, q.rear.data)
-print(q.dequeue())
-print(q.size, q.front.data, q.rear.data)
+q.put("Database")
+q.put("Data structure")
+q.put("Java script")
+print(q.qsize())
+print(q.get())
+print(q.qsize())
+print(q.get())
+print(q.qsize())
 # print(q.size, q.front, q.rear)
 # print(q.dequeue())
 
